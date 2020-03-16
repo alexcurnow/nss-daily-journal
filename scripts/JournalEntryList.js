cd2a6e01f5a2b8b3624b45e1bba1e32f7fa32e0a
Alex Curnow <alexcurnow@gmail.com>
2020-03-16 14:47:05 -0500

    got rid of some commented out code

--- scripts/JournalEntryList.js
+++ scripts/JournalEntryList.js
@@ -1,12 +1,11 @@
-import { useJournalEntries, getJournalEntries, deleteJournalEntry } from "./JournalDataProvider.js"
+import { useJournalEntries, deleteJournalEntry } from "./JournalDataProvider.js"
 import JournalEntryComponent from "./JournalEntry.js"
 
 const entryLog = document.querySelector("#entryLog")
 
 entryLog.addEventListener('click', event => {
   if (event.target.id.startsWith('delete-btn-')) {
-    // const entries = useJournalEntries()
-    // const entryIdSelector = entries
+    
     const [prefix, entryIdNumber] = event.target.parentNode.id.split("--")
     deleteJournalEntry(entryIdNumber)
     event.target.parentNode.style.display = 'none'
